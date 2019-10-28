ba3964650ecb183b4252029fc5e587fec94b5135
Fahad <Fahad@DESKTOP-6QKVVDK>
2019-10-28 15:54:39 +0500

    Parallel testing

---
+++ Demo Test/src/selenium/CrossBrowserTest.java
@@ -0,0 +1,53 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.*;
+
+public class CrossBrowserTest {
+	
+	WebDriver driver = null;
+	
+	@Parameters("browserName")
+	@BeforeTest
+	public void setUp(String browserName) {
+	
+		System.out.println("Browser is  " + browserName);
+		
+		//check browser
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "E:\\Learning\\Selenium Webdriver\\chrome\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "E:\\Learning\\Selenium Webdriver\\firefox\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		else if (browserName.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", "E:\\Learning\\Selenium Webdriver\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+
+	}
+
+	@Test
+	public void test1() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//open web address 
+		driver.get("http://www.google.com");
+		
+	}
+	
+	@AfterTest
+	public void teardown() {
+		driver.close();
+		System.out.println("Test completed ");
+	}
+}
